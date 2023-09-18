058350a9fc6a26fc6f1e798ca6e721b9aa35a367
ElizavetaMozgunova <140318467+ElizavetaMozgunova@users.noreply.github.com>
2023-09-18 16:46:05 +0300

    Update tack_2.1.py

--- lvl_2/tack_2.1.py
+++ lvl_2/tack_2.1.py
@@ -15,3 +15,94 @@
 
 # def maximum(arr):
 #     pass
+arr = [[4,6,2,1,9,63,-134,566],[-52, 56, 30, 29, -54, 0, -110],[42, 54, 65, 87, 0],[5]]
+def vibor(lst):
+  n = len(lst)
+  i = 0
+  while i < n - 1:
+    m = i
+    j = i + 1
+    while  j < n:
+      if lst[j] < lst [m]:
+        m = j
+      j += 1
+    lst[i],lst[m] = lst[m],lst[i]
+    i += 1
+  return lst
+  def bubble_for(lst):
+    n = len(lst)
+  for i in range(n - 1):
+    for j in range (n - i - 1):
+      if lst[j] > lst[j + 1]:
+        lst[j],lst[j+1] = lst[j+1],lst[j]
+  return lst
+def bubble_while(lst):
+  n = len(lst)
+  i = 0
+  while i < n - 1:
+    j = 0
+    while j < n - i - 1:
+      if lst[j] > lst[j+1]:
+        lst[j],lst[j+1] = lst[j+1],lst[j]
+      j += 1
+    i += 1
+  return lst
+def default(lst):
+  lst.sort()
+  return lst
+def minimum(arr):
+    print ('МИНИМАЛЬНЫЕ ЗНАЧЕНИЯ')
+    print ('Сортировка выбором')
+    start_time = datetime.now()
+    for lst in arr:
+      print ('Минимальное значение из списка', lst, vibor(lst)[0])
+    end_time = datetime.now()
+    print ('Продолжительность: {}'.format(end_time - start_time))
+    print ('Сортировка пузырьком через for')
+    start_time = datetime.now()
+    for lst in arr:
+     print ('Минимальное значение из списка',lst,bubble_for(lst)[0])
+    end_time = datetime.now()
+    print ('Продолжительность: {}'.format(end_time - start_time))
+    print ('Сортировка пузрьком через while')
+    start_time = datetime.now()
+    for lst in arr:
+      print ('Минимальное значение из списка', lst,bubble_while(lst)[0])
+    end_time = datetime.now()
+    print ('Продолжительность: {}'.format(end_time - start_time))
+    print ('Стандартная сортировка')
+    start_time = datetime.now()
+    for lst in arr:
+      print ('Минимальное значение из списка', lst,default(lst)[0])
+    end_time = datetime.now()
+    print ('Продолжительность: {}'.format(end_time - start_time))
+def maximum(arr):
+    print ('МАКСИМАЛЬНЫЕ ЗНАЧЕНИЯ')
+    print ('Сортировка выбором')
+    start_time = datetime.now()
+    for lst in arr:
+      print ('Максимальное значение из списка', lst,vibor(lst)[-1])
+    end_time = datetime.now()
+    print ('Продолжительность: {}'.format(end_time - start_time))
+    print ('Сортировка пузрьком через for')
+    start_time = datetime.now()
+    for lst in arr:
+      print ('Максимальное значение из списка', lst,bubble_for(lst)[-1])
+    end_time = datetime.now()
+    print ('Продолжительность: {}'.format(end_time - start_time))
+    print ('Сортировка пузрьком через while')
+    start_time = datetime.now()
+    for lst in arr:
+      print ('Максимальное значение из списка', lst,bubble_while(lst)[-1])
+    end_time = datetime.now()
+    print ('Продолжительность: {}'.format(end_time - start_time))
+    print ('Стандартная сортировка')
+    start_time = datetime.now()
+    for lst in arr:
+      print ('Максимальное значение из списка', lst,default(lst)[-1])
+    end_time = datetime.now()
+    print ('Продолжительность: {}'.format(end_time - start_time))
+def main():
+  print (minimum(arr))
+  print (maximum(arr))
+main()
