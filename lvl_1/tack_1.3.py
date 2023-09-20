10d1f3f0d6cd1ffea29bd7c361294d5a15a1034e
ElizavetaMozgunova <140318467+ElizavetaMozgunova@users.noreply.github.com>
2023-09-20 23:06:23 +0300

    Update tack_1.3.py
    Добавила решение

--- lvl_1/tack_1.3.py
+++ lvl_1/tack_1.3.py
@@ -16,5 +16,31 @@
     # Введите номер месяца: 15
     # Такого месяца нет!
 
-user_input = input('Введите номер месяца:')
-
+user_input = input("Введите, пожалуйста, номер месяца: ")
+month = int(user_input)
+if month == 1:
+    print(' Вы ввели Январь. 31 дней')
+elif month == 2:
+    print('Вы ввели Февраль. 28 дней')
+elif month == 3:
+    print('Вы ввели Март. 31 дней')
+elif month == 4:
+    print('Вы ввели Апрель. 30 дней')
+elif month == 5:
+    print('Вы ввели Май. 31 дней')
+elif month == 6:
+    print('Вы ввели Июнь. 30 дней')
+elif month == 7:
+    print('Вы ввели Июль.31 дней')
+elif month == 8:
+    print('Вы ввели Август. 31 дней')
+elif month == 9:
+    print('Вы ввели Сентябрь.30 дней')
+elif month == 10:
+    print('Вы ввели Октябрь. 31 дней')
+elif month == 11:
+    print('Вы ввели Ноябрь. 30 дней')
+elif month == 12:
+    print('Вы ввели Декабрь. 31 дней')
+else:
+    print('Такого месяца нет!')
