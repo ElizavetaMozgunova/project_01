875aedc35632c79a349131a44619e5977a3ecf86
ElizavetaMozgunova <140318467+ElizavetaMozgunova@users.noreply.github.com>
2023-09-21 23:53:27 +0300

    Update tack_2.2.py
    Загрузила решение

--- lvl_2/tack_2.2.py
+++ lvl_2/tack_2.2.py
@@ -8,3 +8,17 @@
 
 # def quarter_of(month):
 #     pass
+
+
+def quarter_of(month):
+    if month in [1, 2, 3]:
+        return 'является частью первого квартала'
+    elif month in [4, 5, 6]:
+        return 'является частью второго квартала'
+    elif month in [7, 8, 9]:
+        return 'является частью третьего квартала'
+    elif month in [10, 11, 12]:
+        return 'является частью четвертого квартала'
+
+month = int(input("Введите номер месяца: "))
+print(quarter_of(month))
