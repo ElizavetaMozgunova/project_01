7cac291ac4d5a1f26811cd3448dc45af465adbc5
ElizavetaMozgunova <140318467+ElizavetaMozgunova@users.noreply.github.com>
2023-09-28 13:31:31 +0300

    Добавила решение

---
+++ lvl_3/task_3.1.py
@@ -0,0 +1,97 @@
+# Задача 3.1.
+# Создайте класс матрицы (или таблицы).
+# Требования к классу:
+#   - каждая колонка является числом от 1 до n (n любое число, которые вы поставите!)
+#   - в каждой ячейке содержится либо число, либо None
+#   - доступы следующие методы матрицы:
+#       * принимать новые значения, 
+#       * заменять существующие значения, 
+#       * выводить число строк и колонок.
+
+# Пример матрицы 10 на 10 из единиц:
+# [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+# Примечание! 
+#   - новый класс не запрещено строить на базе существующих типов данных: списков, словарей и тд.
+#   - отображать в таблице/матрице название колонки не обязательно!
+#   - использовать готовые классы numpy.array() и pandas.DataFrame() запрещено!
+#   - проявите фантазию :)
+
+
+# import random
+# class Matrix:
+#   def init_(self, rows, columns):
+#     self.rows = rows
+#     self.columns = columns
+#     self.data = [[None for _ in range(columns)for _ in range(rows)]]
+
+#   def set_value(self, row, column, value):
+#     self.data[row][column] = value
+  
+#   def get_value(self, row,column):
+#     return self.data[row][column]
+  
+#   def print_size(self):
+#     print('Число строк:', self.rows)
+#     print('Число колнок:',self.columns)
+
+#     matrix = Matrix(3, 4)
+#     matrix.set_value(0, 0, 1)
+#     matrix.set_value(0, 1, 2)
+#     matrix.set_value(1, 2, 3)
+#     matrix.set_value(2, 3, 4)
+
+#     print(matrix.get_value(0, 1)) 
+#     print(matrix.get_value(1, 2)) 
+#     print(matrix.get_value(2, 3))
+
+#     matrix.print_size()
+
+
+import random
+
+class Matrix:
+
+    def __init__(self, row, col):
+        self.n = row
+        self.matrix = [[random.randrange(0, 10) for a in range(col)] for b in range(row)]
+
+    def print(self):
+        matrix = self.matrix
+        for im in range(len(matrix)):
+            print(matrix[im])
+
+    def find_max(self):
+        max_sum = 0
+        n = self.n
+        m = self.matrix
+        index = -1
+        for x in range(n):
+            if sum(m[x]) > max_sum:
+                max_sum = sum(m[x])
+                index = x
+        print("максимум: %s, индекс: %s" % (max_sum, index))
+        return max_sum, index
+
+    def remove_max_row(self):
+        matrix = self.matrix
+        n = self.n
+        max_value, index = self.find_max()
+        matrix.remove(matrix[index])
+        for im in range(n-1):
+            print(matrix[im])
+
+Matrix(5, 5).print()
+print('-'*100)
+Matrix(5, 5).find_max()
+print('='*100)
+Matrix(5, 5).remove_max_row()
